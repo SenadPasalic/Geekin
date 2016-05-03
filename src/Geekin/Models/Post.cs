a11e70a83a53f2bc88667eae27f1b69a37fa35a1
Senad Pasalic <senadpasalic@live.com>
2016-05-03 16:50:11 +0200

    Tisdag 3/5 slut på jobbet. Jobbar med ny sida Admin/Addpost

---
+++ src/Geekin/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Geekin.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string PostText { get; set; }
+        public string Link { get; set; }
+        //public string PostedByFirstName { get; set; }
+        //public string PostedByLastName { get; set; }
+        public DateTime TimePosted { get; set; }
+        public string UserID { get; set; }
+        //public virtual User User { get; set; }
+    }
+}
---
+++ src/Geekin/ViewModels/AddPostVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Geekin.ViewModels
+{
+    public class AddPostVM
+    {
+        [Display(Name = "Text")]
+        [Required(ErrorMessage = "Skriv in text som skall publiceras.")]
+        public string PostText { get; set; }
+
+        [Display(Name = "Länk")]
+        public string Link { get; set; }
+
+        public DateTime TimePosted { get; set; }
+    }
+}
