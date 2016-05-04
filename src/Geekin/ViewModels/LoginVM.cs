9f4d769b0f71aaaa0c143c5e9b53e8feb0955649
Senad Pasalic <senadpasalic@live.com>
2016-05-04 21:19:01 +0200

    LoginVM.cs (ViewModel)

---
+++ src/Geekin/ViewModels/LoginVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Geekin.ViewModels
+{
+    public class LoginVM
+    {
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "Fyll i din e-postadress.")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Lösenord")]
+        [Required(ErrorMessage = "Fyll i ditt lösenord.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
