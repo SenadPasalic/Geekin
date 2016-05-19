b82b40ae2afeb30327abbcc72fc1252a75707f58
Senad Pasalic <senadpasalic@live.com>
2016-05-19 13:14:54 +0200

    get to laptop

--- src/Geekin/ViewModels/PostListVM.cs
+++ src/Geekin/ViewModels/PostListVM.cs
@@ -8,6 +8,7 @@ namespace Geekin.ViewModels
 {
     public class PostListVM
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
         public string Link { get; set; }
